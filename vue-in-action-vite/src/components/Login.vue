f12aaf1e5caefa00364d87f58d3aa105e2cdad7c
oylx <oylx2008@163.com>
2021-06-02 17:08:26 +0800

    路由守卫

---
+++ vue-in-action-vite/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="onLogin">登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    onLogin() {
+      localStorage.setItem('token', 'some token');
+      this.$router.push(this.$route.query.redirect);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- vue-in-action-vite/src/router/index.js
+++ vue-in-action-vite/src/router/index.js
@@ -3,6 +3,7 @@ import CourseList from '/comps/CourseList.vue';
 import CourseAdd from '/comps/CourseAdd.vue';
 import CourseDetail from '/comps/CourseDetail.vue';
 import NotFound from '/comps/NotFound.vue';
+import Login from '/comps/Login.vue';
 // 1.配置
 const routes = [{
   path: '/',
@@ -11,22 +12,53 @@ const routes = [{
 }, {
   path: '/course',
   component: CourseList,
-  children: [
-    { path: '/course/add', name: 'add', component: CourseAdd, },
-    { path: '/course/:id', name: 'detail', component: CourseDetail, }
-  ],
-},
-  // 下面配置会匹配所有path，匹配内容放入`$route.params.pathMatch`
+  children: [{
+    path: '/course/add',
+    name: 'add',
+    component: CourseAdd,
+  }, {
+    path: '/course/:id',
+    name: 'detail',
+    component: CourseDetail,
+  }],
+}, // 下面配置会匹配所有path，匹配内容放入`$route.params.pathMatch`
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: NotFound,
-  }, // 匹配所有以`/user-`开头path，匹配内容放入`$route.params.afterUser`
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  }
+  // 匹配所有以`/user-`开头path，匹配内容放入`$route.params.afterUser`
   // { path: '/user-:afterUser(.*)', component: UserGeneric },
 ];
 
-// 2.创建实例
-export default createRouter({
+const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  // 判断是否去add
+  if (to.name === 'add') {
+    // 判断是否登录
+    if (localStorage.getItem('token')) {
+      // 已登录
+      next();
+    } else {
+      // 未登录
+      next({
+        path: '/login',
+        query: { redirect: to.path },
+      });
+    }
+  } else {
+    next();
+  }
+});
+
+// 2.创建实例
+export default router;
