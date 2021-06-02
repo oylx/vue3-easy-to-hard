608b23c6e671872c3a7b103afd361777c371133c
oylx <oylx2008@163.com>
2021-06-02 15:36:17 +0800

    init

--- vue-in-action-vite/tests/example.spec.js
+++ vue-in-action-vite/tests/example.spec.js
@@ -1,13 +1,13 @@
-import HelloWorld from 'main/components/HelloWorld.vue';
-import { shallowMount } from '@vue/test-utils';
+import HelloWorld from "main/components/HelloWorld.vue";
+import { shallowMount } from "@vue/test-utils";
 
-describe('aaa', () => {
-  test('should ', () => {
+describe("aaa", () => {
+  test("should ", () => {
     const wrapper = shallowMount(HelloWorld, {
       props: {
-        msg: 'hello,vue3',
+        msg: "hello,vue3",
       },
     });
-    expect(wrapper.text()).toMatch('hello,vue3');
+    expect(wrapper.text()).toBe("hello,vue3");
   });
 });
