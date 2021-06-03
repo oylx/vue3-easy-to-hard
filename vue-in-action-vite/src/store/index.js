6a274e2441f816747c721b722b3db61120c8085c
oylx <oylx2008@163.com>
2021-06-03 16:59:36 +0800

    vuex-plugin

--- vue-in-action-vite/src/store/index.js
+++ vue-in-action-vite/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex';
-import count  from './count';
+import count from './count';
+import vuexPersist from './vuex-persist';
 
 export const store = createStore({
   modules: {
     count,
-  }
+  },
+  plugins: [vuexPersist],
 });
---
+++ vue-in-action-vite/src/store/vuex-persist.js
@@ -0,0 +1,6 @@
+export default function (store) {
+  store.replaceState(JSON.parse(localStorage.getItem('vuex-persist')));
+  store.subscribe((mutation, state) => {
+    localStorage.setItem('vuex-persist', JSON.stringify(state));
+  });
+}
